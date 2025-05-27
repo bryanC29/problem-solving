f5ae0bf01db01a29bd3994afb6087a80ef90b063
Bryan Christy <bryanchristy2016@gmail.com>
2025-05-27 23:08:04 +0530

    Remove duplicate elements from sorted array - Leetcode

---
+++ leetcode/removeDuplicateSortedArray/solution1.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let slow = 0;
+
+    for(let fast = 1; fast < nums.length; fast++) {
+        if(nums[fast] != nums[slow]) {
+            nums[++slow] = nums[fast];
+        }
+    }
+
+    return slow + 1;
+};
