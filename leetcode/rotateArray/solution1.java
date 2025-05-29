eea4bb4b5f4c8d9a0c5942ad331586cea57b4c64
Bryan Christy <bryanchristy2016@gmail.com>
2025-05-29 09:34:32 +0530

    Rotate array - Leetcode

---
+++ leetcode/rotateArray/solution1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(k == 0) {
+            return;
+        }
+
+        int[] ans = new int[nums.length];
+        
+        for(int i = 0; i < nums.length; i++) {
+            ans[( i + k ) % nums.length] = nums[i];
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = ans[i];
+        }
+    }
+}
